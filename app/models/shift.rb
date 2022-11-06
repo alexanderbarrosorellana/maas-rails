90270e83a72496c872a9d7ceb057c6a0732d5677
Alexander Barros <alexander.barros.orellana@gmail.com>
2022-11-06 19:38:25 -0300

    chore: wip shift scopes

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,8 @@ class Shift < ApplicationRecord
   belongs_to :service
   belongs_to :engineer, optional: true
   has_many :engineer_shifts
+
+  scope :by_week, ->(date) { where(date: date.beginning_of_week..date.end_of_week) }
+  scope :by_service, ->(service_id) { where(service_id: service_id) }
+
 end
